60088acf4be24b8a21d2fe6d0eea885e23ff5e8f
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-03-25 15:23:44 +0530

    completed api endpoints

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,15 +1,69 @@
 import express from "express";
 import { authMiddleware } from "./middleware";
+import { prismaClient } from "db/client";
 
 const app = express();
 
-app.post("/api/v1/websites", authMiddleware, async (req, res) => {});
+app.use(express.json());
 
-app.get("/api/v1/website/states", authMiddleware, (req, res) => {
-  const userId = req.userId;
+app.post("/api/v1/websites", authMiddleware, async (req, res) => {
+  const userId = req.userId!;
+  const { url } = req.body;
+  const data = await prismaClient.website.create({
+    data: {
+      userId,
+      url,
+    },
+  });
+  res.json({
+    id: data.id,
+  });
 });
 
-app.put("/api/v1/websites", authMiddleware, (req, res) => {});
+app.get("/api/v1/website/states", authMiddleware, async (req, res) => {
+  const websiteId = req.query.websiteId! as unknown as string;
+  const userId = req.userId;
 
-app.delete("/api/v1/website/", authMiddleware, (req, res) => {});
+  const data = prismaClient.website.findFirst({
+    where: {
+      id: websiteId,
+      userId,
+    },
+    include: {
+      ticks: true,
+    },
+  });
+  res.json(data);
+});
+
+app.put("/api/v1/websites", authMiddleware, async (req, res) => {
+  const userId = req.userId;
+
+  const website = prismaClient.website.findMany({
+    where: {
+      userId,
+      disabled: false,
+    },
+  });
+  res.json({ website });
+});
+
+app.delete("/api/v1/website/", authMiddleware, async (req, res) => {
+  const websitId = req.body.websiteId;
+  const userId = req.userId;
+
+  await prismaClient.website.update({
+    where: {
+      id: websitId,
+      userId,
+    },
+    data: {
+      disabled: true,
+    },
+  });
+
+  res.json({
+    message: "Deleted website successfully!",
+  });
+});
 app.listen(3000);

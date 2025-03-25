de4ab97ec4205b1ef9f8feac48b55d4416b1f912
Sushant Singh Negi <sushantsinghnegi1998@gmail.com>
2025-03-25 14:50:02 +0530

    setting up middleware

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { authMiddleware } from "./middleware";
+
+const app = express();
+
+app.post("/api/v1/websites", authMiddleware, async (req, res) => {});
+
+app.get("/api/v1/website/states", authMiddleware, (req, res) => {
+  const userId = req.userId;
+});
+
+app.put("/api/v1/websites", authMiddleware, (req, res) => {});
+
+app.delete("/api/v1/website/", authMiddleware, (req, res) => {});
+app.listen(3000);
